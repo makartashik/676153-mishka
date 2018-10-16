26ce3959824bb47e136005163743c6f2f4a1f520
Natalia <wowmalish@gmail.com>
2018-10-16 10:51:17 +0300

    Реализует появление и закрытие модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 var pageHeader = document.querySelector(".page-header");
 var navToggle = document.querySelector(".page-header__nav-toggle");
 
+var button = document.querySelectorAll(".button--open-modal");
+var modal = document.querySelector(".modal");
+var wrap = document.querySelector(".modal--wrap");
+
 pageHeader.classList.remove("page-header--nojs");
 
 navToggle.addEventListener("click", function() {
@@ -11,4 +15,29 @@ navToggle.addEventListener("click", function() {
     pageHeader.classList.add("page-header--closed");
     pageHeader.classList.remove("page-header--opened");
   }
+});
+
+for(var i=0;i<button.length;i++) {
+  button[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("modal--show");
+    wrap.classList.add("modal--wrap-show");
+  });
+};
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modal.classList.contains("modal--show")) {
+        modal.classList.remove("modal--show");
+      }
+      if (wrap.classList.contains("modal--wrap-show")) {
+        wrap.classList.remove("modal--wrap-show");
+      }
+    }
+  });
+
+wrap.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal--show");
+  wrap.classList.remove("modal--wrap-show");
 });
